7494a7de7c23a8162fbe3a24ee60764a0e3de2b1
Sunny Gleason <sunny.gleason@gmail.com>
2010-10-01 09:19:51 -0400

    re-enabling validator and create/drop db tests

--- src/test/java/com/g414/inno/db/G414InnoDBCreateDropDBTest.java
+++ src/test/java/com/g414/inno/db/G414InnoDBCreateDropDBTest.java
@@ -1,18 +1,18 @@
-//package com.g414.inno.db;
-//
-//import org.testng.annotations.Test;
-//
-//@Test
-//public class G414InnoDBCreateDropDBTest {
-//    private static String DATABASE_NAME = "foo";
-//
-//    public void testCreateDrop() throws Exception {
-//        Database d = new Database();
-//
-//        d.dropDatabase(DATABASE_NAME + "/");
-//
-//        d.createDatabase(DATABASE_NAME);
-//
-//        d.dropDatabase(DATABASE_NAME + "/");
-//    }
-//}
+package com.g414.inno.db;
+
+import org.testng.annotations.Test;
+
+@Test
+public class G414InnoDBCreateDropDBTest {
+    private static String DATABASE_NAME = "foo";
+
+    public void testCreateDrop() throws Exception {
+        Database d = new Database();
+
+        d.dropDatabase(DATABASE_NAME + "/");
+
+        d.createDatabase(DATABASE_NAME);
+
+        d.dropDatabase(DATABASE_NAME + "/");
+    }
+}
